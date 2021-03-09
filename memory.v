ae5f686f1d5169d8deed1a5d101f3c8dd5ec1ddc
dinesh2212 <dinesh.rajakumar@students.iiit.ac.in>
2021-03-09 20:28:56 +0400

    Memory layout updated

--- memory.v
+++ memory.v
@@ -1,12 +1,10 @@
-module memory(addr, data, read, write, res, clk, valM,
-M1, M2, M3, M4, M5, M6, M7, M8, M9, M10,
-M11, M12, M13, M14, M15, M16, M17, M18, M19, M20
-);
+module memory(addr, data, read, write, clk, valM);
 
 input [63:0] addr, data;
-input read, write, res;
+input read, write, res, clk;
 //output memerr;
-output [63:0] valM, M1, M2, M3, M4, M5, M6, M7, M8, M9, M10,
+output [63:0] valM;
+wire [63:0] M1, M2, M3, M4, M5, M6, M7, M8, M9, M10,
 M11, M12, M13, M14, M15, M16, M17, M18, M19, M20;
 
 wire [63:0] mvalM, M1_dat, M2_dat, M3_dat, M4_dat, M5_dat, M6_dat,
@@ -17,26 +15,26 @@ wire M1_wrt, M2_wrt, M3_wrt, M4_wrt, M5_wrt, M6_wrt,
 M7_wrt, M8_wrt, M9_wrt, M10_wrt, M11_wrt, M12_wrt, M13_wrt,
 M14_wrt, M15_wrt, M16_wrt, M17_wrt, M18_wrt, M19_wrt, M20_wrt;
 
-clockreg # (64) M1reg(M1, M1_dat, M1_wrt, res, 64'b0, clk);
-clockreg # (64) M2reg(M2, M2_dat, M2_wrt, res, 64'b0, clk);
-clockreg # (64) M3reg(M3, M3_dat, M3_wrt, res, 64'b0, clk);
-clockreg # (64) M4reg(M4, M4_dat, M4_wrt, res, 64'b0, clk);
-clockreg # (64) M5reg(M5, M5_dat, M5_wrt, res, 64'b0, clk);
-clockreg # (64) M6reg(M6, M6_dat, M6_wrt, res, 64'b0, clk);
-clockreg # (64) M7reg(M7, M7_dat, M8_wrt, res, 64'b0, clk);
-clockreg # (64) M8reg(M8, M8_dat, M8_wrt, res, 64'b0, clk);
-clockreg # (64) M9reg(M9, M9_dat, M9_wrt, res, 64'b0, clk);
-clockreg # (64) M10reg(M10, M10_dat, M10_wrt, res, 64'b0, clk);
-clockreg # (64) M11reg(M11, M11_dat, M11_wrt, res, 64'b0, clk);
-clockreg # (64) M12reg(M12, M12_dat, M12_wrt, res, 64'b0, clk);
-clockreg # (64) M13reg(M13, M13_dat, M13_wrt, res, 64'b0, clk);
-clockreg # (64) M14reg(M14, M14_dat, M14_wrt, res, 64'b0, clk);
-clockreg # (64) M15reg(M15, M15_dat, M15_wrt, res, 64'b0, clk);
-clockreg # (64) M16reg(M16, M16_dat, M16_wrt, res, 64'b0, clk);
-clockreg # (64) M17reg(M17, M17_dat, M17_wrt, res, 64'b0, clk);
-clockreg # (64) M18reg(M18, M18_dat, M18_wrt, res, 64'b0, clk);
-clockreg # (64) M19reg(M19, M19_dat, M19_wrt, res, 64'b0, clk);
-clockreg # (64) M20reg(M20, M20_dat, M20_wrt, res, 64'b0, clk);
+clockreg # (64) M1reg(M1, M1_dat, M1_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M2reg(M2, M2_dat, M2_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M3reg(M3, M3_dat, M3_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M4reg(M4, M4_dat, M4_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M5reg(M5, M5_dat, M5_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M6reg(M6, M6_dat, M6_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M7reg(M7, M7_dat, M8_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M8reg(M8, M8_dat, M8_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M9reg(M9, M9_dat, M9_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M10reg(M10, M10_dat, M10_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M11reg(M11, M11_dat, M11_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M12reg(M12, M12_dat, M12_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M13reg(M13, M13_dat, M13_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M14reg(M14, M14_dat, M14_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M15reg(M15, M15_dat, M15_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M16reg(M16, M16_dat, M16_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M17reg(M17, M17_dat, M17_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M18reg(M18, M18_dat, M18_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M19reg(M19, M19_dat, M19_wrt, 1'b0, 64'b0, clk);
+clockreg # (64) M20reg(M20, M20_dat, M20_wrt, 1'b0, 64'b0, clk);
 
 assign mvalM = addr == 1 ? M1 :
 addr == 2 ? M2 :
@@ -46,8 +44,8 @@ addr == 5 ? M5 :
 addr == 6 ? M6 :
 addr == 7 ? M7 :
 addr == 8 ? M8 :
-addr == 9 : M9 :
-addr == 10 : M10 :
+addr == 9 ? M9 :
+addr == 10 ? M10 :
 addr == 11 ? M11 :
 addr == 12 ? M12 :
 addr == 13 ? M13 :
