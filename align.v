13f1daab7113a19f25b0e26f79e1c6dfb98d2da3
dinesh2212 <dinesh.rajakumar@students.iiit.ac.in>
2021-03-09 20:30:00 +0400

    RNONE register case fixed

--- align.v
+++ align.v
@@ -6,8 +6,8 @@ output [3:0] rA, rB;
 output [63:0] valC;
 input needregid;
 
-assign rA = needregid ? inst[71:68] : 0;
-assign rB = needregid ? inst[67:64] : 0;
+assign rA = needregid ? inst[71:68] : 15;
+assign rB = needregid ? inst[67:64] : 15;
 
 assign valC = inst[63:0];
 endmodule
